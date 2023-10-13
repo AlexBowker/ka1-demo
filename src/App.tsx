bdf83d9dd0658fab3e73c60ed47261ab5dafa1fb
Alex Bowker <alex.bowker@salfordcc.ac.uk>
2023-10-13 11:08:47 +0100

    Finish task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import ToDoList from "./components/ToDoList";
+
 function App() {
   return (
     <>
+      <ToDoList title="Shopping List" items={["Milk", "Cheese", "Bread", "Ham", "Butter"]} />
+
+      <ToDoList title="Chore List" items={["Vacuum", "Wash Dishes", "Laundry"]} />
     </>
   )
 }
